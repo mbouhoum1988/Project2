22a1afaeae50eea25208e5ae814f2190a10fc9fc
mouhoub bouhoum <mouhoub@mouhoubs-MacBook-Air.local>
2019-03-19 18:38:07 -0500

    add database

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,32 +2,32 @@ var db = require("../models");
 var path = require("path");
 
 module.exports = function(app) {
-
   app.get("/", function(req, res) {
-    // db.Example.findAll({}).then(function() {
-      res.sendFile(path.join(__dirname, "../public/assets/index.html"));
-    });
-  // });
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+  });
 
   app.get("/recipeSearch", function(req, res) {
-    // db.Example.findOne().then(function() {
     res.sendFile(path.join(__dirname, "../public/assets/recipesearch.html"));
-    // });
   });
 
   app.get("/login", function(req, res) {
-    // db.Example.findAll({}).then(function() {
-      res.sendFile(path.join(__dirname, "../public/assets/login.html"));
-    });
-  // });
+    res.sendFile(path.join(__dirname, "../public/assets/login.html"));
+  });
 
   app.get("/register", function(req, res) {
-    // db.Example.findAll({}).then(function() {
-      res.sendFile(path.join(__dirname, "../public/assets/signup.html"));
-    });
-  // });
+    res.sendFile(path.join(__dirname, "../public/assets/signup.html"));
+  });
 
   app.get("*", function(req, res) {
     res.redirect("/");
+  });
+
+  app.post("/api/food", function(req, res) {
+    db.Example.create({
+      text: req.body.text,
+      description: req.body.description
+    }).then(function(addfood) {
+      res.json(addfood);
+    });
   });
 };
